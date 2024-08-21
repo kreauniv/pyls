c07747866f9b0fc619baf8c4f8a9e0e27f948827
Srikumar Subramanian <srikumar.subramanian@krea.edu.in>
2024-08-21 08:09:26 +0530

    Fixed #2 - Added -F flag to list file type using character suffix.
    Used `os.access` and `os.path.isdir` for checking the types.
    UNTESTED: Tested by hand, but no formal tests yet.

--- pyls.py
+++ pyls.py
@@ -15,6 +15,13 @@ parser.add_argument(
     default=".",
 )
 
+parser.add_argument(
+    "-F",
+    "--filetype",
+    help="Describes file type by appending an extra character at end of file name.",
+    action="store_true",
+)
+
 
 def main():
     """
@@ -24,19 +31,53 @@ def main():
     directory.
     """
     args = parser.parse_args()
-    print_listing(args.dirname)
+    print_listing(args.dirname, args.filetype)
 
 
-def print_listing(dirname):
+def print_listing(dirname, show_filetype):
     """
     Lists contents of a directory to stdout.
     :param dirname: A directory whose contents are to be listed.
+    :param show_filetype: Appends an extra character to file name showing its type.
     """
     files = get_directory_listing(dirname)
     assert type(files) == list
+    if show_filetype:
+        files = describe_files(dirname, files)
 
     for f in files:
         print(f)
+
+
+def describe_files(dirname, files):
+    """
+    :param dirname: Take the directory whose listing is given in `files`.
+    :param files: List of files in `dirname`.
+    :returns: List of files with extra character at end that captures file type.
+    Uses '*' for executables and '/' for directories.
+    """
+    assert os.path.isdir(dirname)
+
+    return [describe_file(dirname, filename) for filename in files]
+
+
+def describe_file(dirname, filename):
+    """
+    (See `describe_files` - but for one file)
+    """
+    return filename + filetype_char(dirname, filename)
+
+
+def filetype_char(dirname, filename):
+    """
+    (See `describe_files`)
+    """
+    fullpath = os.path.join(dirname, filename)
+    if os.path.isdir(fullpath):
+        return "/"
+    if os.access(fullpath, os.X_OK):
+        return "*"
+    return ""
 
 
 def get_directory_listing(dirname):
